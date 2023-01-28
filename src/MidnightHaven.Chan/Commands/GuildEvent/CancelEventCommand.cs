ff0d4c8708fb96ef211659d52d99bd3c039813f9
Twinki14 <thetwinkehh@gmail.com>
2023-01-28 11:43:17 -0500

    wip

---
+++ src/MidnightHaven.Chan/Commands/GuildEvent/CancelEventCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace MidnightHaven.Chan.Commands.GuildEvent;
+
+public class CancelEventCommand
+{
+
+}
--- src/MidnightHaven.Chan/Startup.cs
+++ src/MidnightHaven.Chan/Startup.cs
@@ -2,7 +2,6 @@
 using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
-using MidnightHaven.Chan.Consumers;
 using MidnightHaven.Chan.Consumers.GuildEvents;
 using MidnightHaven.Chan.Services;
 using SlimMessageBus.Host.Memory;
@@ -17,31 +16,16 @@ public static class Startup
         services.AddBackgroundServices();
         services.AddDiscordDotNet();
 
-            /*// Any consumer events for the bus (a sum of all events across registered consumers)
-            .AttachEvents(events =>
-        {
-            // Invoke the action for the specified message type when sent via the bus:
-            events.OnMessageArrived = (bus, consumerSettings, message, path, nativeMessage) => {
-                Console.WriteLine("The message: {0} arrived on the topic/queue {1}", message, path);
-            };
-
-            events.OnMessageFault = (bus, consumerSettings, message, ex, nativeMessage) => {
-            };
-
-            events.OnMessageFinished = (bus, consumerSettings, message, path, nativeMessage) => {
-                Console.WriteLine("The SomeMessage: {0} finished on the topic/queue {1}", message, path);
-            }
-        });*/
         services.AddSlimMessageBus(mb =>
         {
             mb
-                .Produce<IGuildScheduledEvent>(x => x.DefaultTopic("test"))
+                .Produce<IGuildScheduledEvent>(x => x.DefaultTopic("guildevent"))
                 .Consume<IGuildScheduledEvent>(x => x
-                    .Topic(Topics.GuildEvent.Created)
-                    .WithConsumer<GuildEventCreatedConsumer>())
+                    .Topic(Topics.GuildEvent.Cancelled).WithConsumer<GuildEventCancelledConsumer>())
                 .Consume<IGuildScheduledEvent>(x => x
-                    .Topic(Topics.GuildEvent.Cancelled)
-                    .WithConsumer<GuildEventCancelledConsumer>())
+                    .Topic(Topics.GuildEvent.Created).WithConsumer<GuildEventCreatedConsumer>())
+                .Consume<IGuildScheduledEvent>(x => x
+                    .Topic(Topics.GuildEvent.Started).WithConsumer<GuildEventStartedConsumer>())
 
                 .WithProviderMemory();
         }, addConsumersFromAssembly: new[] { Assembly.GetExecutingAssembly() }); // Auto discover consumers and register inside DI container);
