4b77aeec9ccd4df6f8000744ae1c1b3635443ba8
Twinki14 <thetwinkehh@gmail.com>
2023-01-30 23:55:35 -0500

    logging improvements

--- src/MidnightHaven.Chan/Program.cs
+++ src/MidnightHaven.Chan/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace MidnightHaven.Chan;
 
@@ -8,14 +10,29 @@ public static class Program
 {
     private static async Task Main(string[] args)
     {
-        using var host = CreateHostBuilder(args).Build();
-        await host.RunAsync();
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .WriteTo.Console(new CompactJsonFormatter())
+            .CreateLogger();
+
+        try
+        {
+            using var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            await Log.CloseAndFlushAsync();
+        }
     }
 
     private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
         .ConfigureAppConfiguration(builder => builder.AddEnvironmentVariables())
         .ConfigureServices(Startup.ConfigureServices)
-        .UseSerilog((_, config) => config
-            .Enrich.FromLogContext()
-            .WriteTo.Console());
+        .UseSerilog();
 }
