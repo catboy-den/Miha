536f83a9c19e3c288a1b75efef999d7e49f605c7
Twinki14 <thetwinkehh@gmail.com>
2023-04-02 14:39:39 -0400

    init birthday services

---
+++ src/MidnightHaven.Chan/Services/Birthday/BirthdayAnnouncementService.cs
@@ -0,0 +1,9 @@
+namespace MidnightHaven.Chan.Services.Birthday;
+
+/// <summary>
+/// Scans for BirthdayJobDocuments
+/// </summary>
+public class BirthdayAnnouncementService
+{
+
+}
---
+++ src/MidnightHaven.Chan/Services/Birthday/BirthdayScannerService.cs
@@ -0,0 +1,9 @@
+namespace MidnightHaven.Chan.Services.Birthday;
+
+/// <summary>
+/// Scans birthdays, and creates BirthdayJobDocuments
+/// </summary>
+public class BirthdayScannerService
+{
+
+}
