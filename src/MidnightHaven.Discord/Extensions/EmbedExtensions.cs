f24e30fb3a92a5be762247ba752165e61868fe09
Twinki <Twinki14@users.noreply.github.com>
2023-06-28 00:20:03 -0400

    switch to gitinfo

--- src/MidnightHaven.Discord/Extensions/EmbedExtensions.cs
+++ src/MidnightHaven.Discord/Extensions/EmbedExtensions.cs
@@ -5,5 +5,5 @@ namespace MidnightHaven.Discord.Extensions;
 public static class EmbedExtensions
 {
     public static EmbedBuilder WithVersionFooter(this EmbedBuilder builder) =>
-        builder.WithFooter("v" + ThisAssembly.AssemblyInformationalVersion);
+        builder.WithFooter("v" + ThisAssembly.Git.Commit);
 }
